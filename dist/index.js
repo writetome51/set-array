cd043ee20a87efde5c5fefaf8bc40d177f72ecc3
Steve Thompson <writetome51@gmail.com>
2020-08-10 21:42:57 -0600

    Refactored code to optimize performance. Behavior is same. Committing version 2.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,40 @@
 import { errorIfValuesAreNotArrays } from 'error-if-values-are-not-arrays';
-/****
- This assigns a new value to an array without breaking its memory reference.
- It's a much better alternative to doing this...
 
- array = newArray;
 
- ...which would break the memory reference.
- ****/
+// This assigns a new value to an array without breaking its memory reference.
+
 export function setArray(array, newArray) {
     errorIfValuesAreNotArrays([array, newArray]);
 
-    // These 2 lines make sure any memory reference to array is not broken:
+    let length = newArray.length;
+    array.length = length;
+    let i = -1;
+    while (++i < length) array[i] = newArray[i];
+}
+
+
+/********************************
+function setArray(array, newArray) {
+    // avg 2.6ms using this method:
     array.length = 0;
     array.push(...newArray);
+
+
+    // avg 1.96ms using this new method:
+    let length = newArray.length;
+    array.length = length;
+    let i = -1;
+    while (++i < length) array[i] = newArray[i];
 }
+
+let i = -1, newArr = [], oldArr = [];
+while (++i < 100000){
+    newArr[i] = i;
+    oldArr[i] = i + 1;
+}
+
+console.time('check');
+setArray(oldArr, newArr);
+console.timeEnd('check');
+
+*******************************/
